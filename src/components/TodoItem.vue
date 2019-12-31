5e2ee778900e1f5e488abd0b17af034348459573
Димитров Олег Анатольевич <olandi92@mail.ru>
2019-12-31 14:34:01 +0300

    end https://www.youtube.com/watch?v=OlnwgS-gk8Y

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -3,7 +3,7 @@
         <span v-bind:class="{done: todo.completed}">
         <input type="checkbox" v-on:change="todo.completed = !todo.completed">
             <strong>{{index+ 1}}</strong>
-             {{todo.title}}
+             {{todo.title | uppercase}}
         </span>
         <button class="rm" v-on:click="$emit('remove-todo',todo.id)">&times;</button>
 
@@ -18,6 +18,11 @@
                 required: true
             },
             index: Number
+        },
+        filters:{
+            uppercase(value){
+                return value.toUpperCase()
+            }
         }
 
     }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,6 @@
     <div>
         <h2>Home page</h2>
         <p>1234567890</p>
-        <a href="/todos">Todos</a>
+        <router-link to="/todos">Todos</router-link>
     </div>
 </template>
